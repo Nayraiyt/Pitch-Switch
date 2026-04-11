d72256837e0ead313b8375283c287cf8ebcdd89f
Nayraiyt <naomimorganc@gmail.com>
2026-04-11 17:57:32 -0400

    removed node.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('public'));
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('App available on http://localhost:3000');
-});
